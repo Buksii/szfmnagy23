20c5b2fab2a14c35419df7a96e6bf00b5784b880
Bence Csengeri <bencecsengeri@gmail.com>
2023-10-29 12:34:27 +0100

    Skeleton upgraded for libraries, wget, os added

---
+++ Code/linear_reg_main.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.metrics import mean_squared_error
+
+import wget
+import os
+
+import datetime
