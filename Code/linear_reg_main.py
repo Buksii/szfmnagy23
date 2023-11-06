3e4a7b081f17448cbbb0b9dffaf1c40861d764f1
albertbokor-unideb <128703802+albertbokor-unideb@users.noreply.github.com>
2023-11-06 08:13:37 +0100

    Removed unused wget import

--- Code/linear_reg_main.py
+++ Code/linear_reg_main.py
@@ -6,7 +6,6 @@ from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.metrics import mean_squared_error
 
-import wget
 import os
 
 import datetime
