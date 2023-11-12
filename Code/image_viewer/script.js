9222e008d18777e425faf182d88978f0e65d14e3
Szepesi Imre <imijuhu@gmail.com>
2023-11-12 18:40:46 +0100

    onload

--- Code/image_viewer/script.js
+++ Code/image_viewer/script.js
@@ -1,10 +1,12 @@
-   document.querySelectorAll('.image').forEach(image => {
-             image.addEventListener('click', event => {
-                document.getElementById('full-image').setAttribute('src', image.getAttribute('src'));
-                document.getElementById('image-viewer').style.display = 'block';
-            })
+window.onload = function() {
+    document.querySelectorAll('.image').forEach(image => {
+        image.addEventListener('click', event => {
+            document.getElementById('full-image').setAttribute('src', image.getAttribute('src'));
+            document.getElementById('image-viewer').style.display = 'block';
         })
+    })
 
-     document.getElementById('close').addEventListener('click', function() {
-            document.getElementById('image-viewer').style.display = 'none';
-        })
+    document.getElementById('close').addEventListener('click', function() {
+        document.getElementById('image-viewer').style.display = 'none';
+    })
+};
