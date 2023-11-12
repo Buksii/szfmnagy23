3c5c3d60aaf8203f7f196d16fe4a339a2b070225
Szepesi Imre <imijuhu@gmail.com>
2023-11-12 18:35:57 +0100

    script folytatása

--- Code/image_viewer/script.js
+++ Code/image_viewer/script.js
@@ -1,4 +1,6 @@
    document.querySelectorAll('.image').forEach(image => {
-
+             image.addEventListener('click', event => {
+                document.getElementById('full-image').setAttribute('src', image.getAttribute('src'));
+                document.getElementById('image-viewer').style.display = 'block';
             })
         })
